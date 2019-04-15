2c2b1ee853cbb2fc36b625691ccc326bc6ab6262
shunkaiz <shunkaiz@usc.edu>
2019-04-14 21:42:41 -0700

    Updates

---
+++ precache-manifest.1adc46f0344884ae67b29121bc3d49ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5eec4e61754abf89124df236b87358",
+    "url": "/Roomlink-Frontend/static/media/Raleway-Thin.9f5eec4e.ttf"
+  },
+  {
+    "revision": "430a0518f5ff3b6c8968b759a29b36e2",
+    "url": "/Roomlink-Frontend/static/media/Raleway-Medium.430a0518.ttf"
+  },
+  {
+    "revision": "b1bdea561f247adc2c904f5b24a07c51",
+    "url": "/Roomlink-Frontend/static/media/Raleway-Light.b1bdea56.ttf"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/Roomlink-Frontend/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "7cc8f7b8dd3301a45119",
+    "url": "/Roomlink-Frontend/static/js/runtime~main.42401caf.js"
+  },
+  {
+    "revision": "356d7413dbc2f63d9970",
+    "url": "/Roomlink-Frontend/static/js/main.e8f27c79.chunk.js"
+  },
+  {
+    "revision": "224306e603de7863b280",
+    "url": "/Roomlink-Frontend/static/js/2.3b621129.chunk.js"
+  },
+  {
+    "revision": "356d7413dbc2f63d9970",
+    "url": "/Roomlink-Frontend/static/css/main.fd553984.chunk.css"
+  },
+  {
+    "revision": "deda8d2c2a0778129211c5640338863a",
+    "url": "/Roomlink-Frontend/index.html"
+  }
+];
